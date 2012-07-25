0da99404c7f6922bb410a7b639e12a1947d01f5f
Luis Alberto Cisneros Alvarez <luisaca87@hotmail.com>
2012-07-24 21:18:27 -0500

    Versión Original

---
+++ ValidaEstructuraCFDComplemento/Form1.Designer.vb
@@ -0,0 +1,118 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox
+        Me.Button1 = New System.Windows.Forms.Button
+        Me.Button2 = New System.Windows.Forms.Button
+        Me.TextBox1 = New System.Windows.Forms.TextBox
+        Me.TextBox2 = New System.Windows.Forms.TextBox
+        Me.Label1 = New System.Windows.Forms.Label
+        Me.OpenFileDialog1 = New System.Windows.Forms.OpenFileDialog
+        Me.GroupBox1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.TextBox2)
+        Me.GroupBox1.Location = New System.Drawing.Point(12, 61)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(643, 189)
+        Me.GroupBox1.TabIndex = 0
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "Detalle Validación"
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(469, 10)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(126, 23)
+        Me.Button1.TabIndex = 1
+        Me.Button1.Text = "Seleccionar XML"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(212, 39)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(229, 23)
+        Me.Button2.TabIndex = 2
+        Me.Button2.Text = "Validar"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(67, 13)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(396, 20)
+        Me.TextBox1.TabIndex = 3
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(6, 19)
+        Me.TextBox2.Multiline = True
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.ScrollBars = System.Windows.Forms.ScrollBars.Vertical
+        Me.TextBox2.Size = New System.Drawing.Size(631, 164)
+        Me.TextBox2.TabIndex = 3
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(29, 15)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(32, 13)
+        Me.Label1.TabIndex = 4
+        Me.Label1.Text = "XML:"
+        '
+        'OpenFileDialog1
+        '
+        Me.OpenFileDialog1.FileName = "OpenFileDialog1"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(667, 262)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Name = "Form1"
+        Me.Text = "Validación CFD Complementos"
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents GroupBox1 As System.Windows.Forms.GroupBox
+    Friend WithEvents TextBox2 As System.Windows.Forms.TextBox
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents Button2 As System.Windows.Forms.Button
+    Friend WithEvents TextBox1 As System.Windows.Forms.TextBox
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents OpenFileDialog1 As System.Windows.Forms.OpenFileDialog
+
+End Class
---
+++ ValidaEstructuraCFDComplemento/Form1.vb
@@ -0,0 +1,77 @@
+﻿Imports System.IO
+
+Public Class Form1
+
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+        Me.TextBox1.Text = ""
+        Me.OpenFileDialog1.Filter = "Archivos XML (*.xml)|*.xml"
+        Me.OpenFileDialog1.FilterIndex = 1
+        Me.OpenFileDialog1.RestoreDirectory = True
+
+        If Me.OpenFileDialog1.ShowDialog() = System.Windows.Forms.DialogResult.OK Then
+            Try
+                Me.TextBox1.Text = OpenFileDialog1.FileName
+            Catch Ex As Exception
+                MessageBox.Show("Cannot read file from disk. Original error: " & Ex.Message)
+            Finally
+
+            End Try
+        End If
+    End Sub
+
+    Private Sub Button2_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button2.Click
+        Dim context1 As New System.Xml.XmlParserContext(Nothing, Nothing, "", System.Xml.XmlSpace.None)
+        Dim settings As System.Xml.XmlReaderSettings = New System.Xml.XmlReaderSettings()
+        Dim reader As System.Xml.XmlReader
+        Dim DIR_SAT As String = Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly.Location) & "\ArchivosSAT\"
+        Dim xmlDoc As New System.Xml.XmlDocument
+        Dim Comprobante As System.Xml.XmlNode
+
+        Dim NamespaceAddenda As String = ""
+        Dim NamespaceAddendaURL As String = ""
+
+        Try
+            xmlDoc.Load(Me.TextBox1.Text)
+
+            Comprobante = xmlDoc.Item("cfdi:Comprobante")
+            If Not Comprobante("Addenda") Is Nothing Then                
+                If Comprobante("Addenda").ChildNodes.Count > 0 Then
+                    NamespaceAddenda = Comprobante("Addenda").ChildNodes(0).NamespaceURI
+                    NamespaceAddendaURL = (Comprobante.Attributes("xsi:schemaLocation").Value)
+                    NamespaceAddendaURL = NamespaceAddendaURL.Replace("http://www.sat.gob.mx/sitio_internet/cfd/2/cfdv2.xsd", "")
+                    NamespaceAddendaURL = NamespaceAddendaURL.Replace("http://www.sat.gob.mx/cfd/2", "")
+                    NamespaceAddendaURL = NamespaceAddendaURL.Replace(NamespaceAddenda, "").Trim
+                End If
+            End If
+            settings.Schemas.Add("http://www.sat.gob.mx/cfd/3", DIR_SAT & "cfdv3.xsd")
+            settings.Schemas.Add("http://www.sat.gob.mx/detallista", DIR_SAT & "detallista.xsd")
+            settings.Schemas.Add("http://www.sat.gob.mx/divisas", DIR_SAT & "Divisas.xsd")
+            settings.Schemas.Add("http://www.sat.gob.mx/donat", DIR_SAT & "donat.xsd")
+            settings.Schemas.Add("http://www.sat.gob.mx/ecb", DIR_SAT & "ecb.xsd")
+            settings.Schemas.Add("http://www.sat.gob.mx/ecc", DIR_SAT & "ecc.xsd")
+            settings.Schemas.Add("http://www.sat.gob.mx/implocal", DIR_SAT & "implocal.xsd")
+            settings.Schemas.Add("http://www.sat.gob.mx/iedu", DIR_SAT & "iedu.xsd")
+            settings.Schemas.Add("http://www.sat.gob.mx/TimbreFiscalDigital", DIR_SAT & "TimbreFiscalDigital.xsd")            
+
+            If NamespaceAddenda <> "" And NamespaceAddendaURL <> "" Then
+                settings.Schemas.Add(NamespaceAddenda, NamespaceAddenda + NamespaceAddendaURL)
+            End If
+            settings.ValidationType = System.Xml.ValidationType.Schema
+            settings.ValidationFlags = System.Xml.Schema.XmlSchemaValidationFlags.ProcessInlineSchema
+            settings.XmlResolver = Nothing
+
+            reader = System.Xml.XmlReader.Create(Me.TextBox1.Text, settings)
+
+            While (reader.Read())
+                'If reader.Name = "Addenda" Then Exit While
+            End While
+            Me.TextBox2.Text = "Estructura Valida...."
+        Catch ex As System.Xml.Schema.XmlSchemaValidationException
+            Me.TextBox2.Text = ex.Message
+        Finally
+            reader = Nothing
+            settings = Nothing
+        End Try
+
+    End Sub
+End Class
